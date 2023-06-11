d7812910e03fd90f85c74ecfacb8cde323fad592
ahmed zidan <zidanzezo17@gmail.com>
2023-06-11 19:33:22 +0300

    Add project files.

---
+++ BarcodeGenerator/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace BarcodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBarcode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBarcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBarcode
+            // 
+            this.picBarcode.BackColor = System.Drawing.Color.White;
+            this.picBarcode.Location = new System.Drawing.Point(29, 13);
+            this.picBarcode.Name = "picBarcode";
+            this.picBarcode.Size = new System.Drawing.Size(577, 227);
+            this.picBarcode.TabIndex = 0;
+            this.picBarcode.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 265);
+            this.Controls.Add(this.picBarcode);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BarcodeGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.picBarcode)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBarcode;
+    }
+}
+
---
+++ BarcodeGenerator/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using ZXing;
+
+namespace BarcodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        private IKeyboardMouseEvents globalMouseHook;
+        public Form1()
+        {
+            InitializeComponent();
+            // Note: for the application hook, use the Hook.AppEvents() instead.
+            globalMouseHook = Hook.GlobalEvents();
+
+            // Bind MouseDoubleClick event with a function named MouseDoubleClicked.
+            globalMouseHook.MouseDoubleClick += MouseDoubleClicked;
+
+            // Bind DragFinished event with a function.
+            // Same as double click, so I didn't write here.
+            //globalMouseHook.MouseDragFinished += MouseDragFinished;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.KeyPreview = true;
+        }
+
+
+        // I make the function async to avoid GUI lags.
+        private async void MouseDoubleClicked(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            // Save clipboard's current content to restore it later.
+            IDataObject tmpClipboard = Clipboard.GetDataObject();
+
+            Clipboard.Clear();
+
+            // I think a small delay will be more safe.
+            // You could remove it, but be careful.
+            await Task.Delay(50);
+
+            // Send Ctrl+C, which is "copy"
+            System.Windows.Forms.SendKeys.SendWait("^c");
+
+            // Same as above. But this is more important.
+            // In some softwares like Word, the mouse double click will not select the word you clicked immediately.
+            // If you remove it, you will not get the text you selected.
+            await Task.Delay(50);
+
+            if (Clipboard.ContainsText())
+            {
+                string text = Clipboard.GetText();
+
+                BarcodeWriter writer = new BarcodeWriter() { Format = BarcodeFormat.CODE_128 };
+                
+                picBarcode.Image = writer.Write(text);
+                picBarcode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+
+                // Your code                                                                                                                                    
+
+
+
+            }
+            else
+            {
+                // Restore the Clipboard.
+                Clipboard.SetDataObject(tmpClipboard);
+            }
+        }
+
+
+
+
+    }
+}
